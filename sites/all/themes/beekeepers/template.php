862c92e527304ead5ee867ac71b4c5c4ffb2727a
James Scott <james@velocitymadegood.co.nz>
2015-09-10 22:25:12 +1200

    Add Drupal 7 codebase and beekeeper required modules

---
+++ sites/all/themes/beekeepers/template.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @file
+ * Template.php - process theme data for your sub-theme.
+ */
+
+
+/**
+ * Override or insert variables for the html template.
+ */
+/* -- Delete this line if you want to use this function
+function beekeepers_preprocess_html(&$vars) {
+}
+function beekeepers_process_html(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables for the page templates.
+ */
+/* -- Delete this line if you want to use these functions
+function beekeepers_preprocess_page(&$vars) {
+}
+function beekeepers_process_page(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the node templates.
+ */
+/* -- Delete this line if you want to use these functions
+function beekeepers_preprocess_node(&$vars) {
+}
+function beekeepers_process_node(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the comment templates.
+ */
+/* -- Delete this line if you want to use these functions
+function beekeepers_preprocess_comment(&$vars) {
+}
+function beekeepers_process_comment(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the block templates.
+ */
+/* -- Delete this line if you want to use these functions
+function beekeepers_preprocess_block(&$vars) {
+}
+function beekeepers_process_block(&$vars) {
+}
+// */
